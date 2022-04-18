62854bf6ff35fdf8a28d879e6cbb7a4e11bbe89b
diserran <102823601+diserran@users.noreply.github.com>
2022-04-18 22:49:29 +0200

    updated ft_split

--- ft_split.c
+++ ft_split.c
@@ -12,17 +12,57 @@
 
 #include "libft.h"
 
-/* char	ft_count()
+static int	ft_splitcount(const char *s, char c)
 {
+	int		words;
+	int		i;
 
-} */
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			words++;
+		while (s[i] != c && s[i])
+			i++;
+	}
+	return (words);
+}
+
+static char	**ft_splitlogic(const char *s, char c)
+{
+	int		i;
+	int		j;
+	int		start;
+	int		length;
+	char	**str;
+
+	i = -1;
+	j = 0;
+	start = -1;
+	length = ft_strlen(s);
+	str = (char **) malloc(sizeof(char *) * (ft_splitcount(s, c) + 1));
+	if (!str || sizeof(str) == 0)
+		return (NULL);
+	while (++i <= length)
+	{
+		if (s[i] != c && start < 0)
+			start = i;
+		else if (start >= 0 && (s[i] == c || i == length))
+		{
+			str[j++] = ft_substr(s, start, (i - start));
+			start = -1;
+		}
+	}
+	str[j] = NULL;
+	return (str);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	char	*str;
-	char	b;
-
-	str = (char *)s;
-	b = c;
-	return (NULL);
+	if (!s)
+		return (NULL);
+	return (ft_splitlogic(s, c));
 }
