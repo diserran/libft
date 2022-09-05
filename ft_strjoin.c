bf0d7625b6e9d0dd5b828d88f89db309236ebcee
Diego Serrano Varela <diserran@student.42urduliz.com>
2022-09-05 14:28:33 +0200

    improved ft_strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: diserran <diserran@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 18:40:53 by diserran          #+#    #+#             */
-/*   Updated: 2022/04/15 14:17:23 by diserran         ###   ########.fr       */
+/*   Updated: 2022/09/05 14:26:56 by diserran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,22 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	int		len_s1;
+	int		size;
 	int		i;
+	int		j;
 
 	if (!s1 || !s2)
 		return (NULL);
-	i = 0;
-	len_s1 = ft_strlen(s1);
-	str = malloc(sizeof(*s1) * ((len_s1 + ft_strlen(s2)) + 1));
+	size = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * (size + 1));
 	if (!str)
 		return (NULL);
-	while (s1[i])
-	{
+	i = -1;
+	while (s1[++i])
 		str[i] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (s2[i])
-	{
-		str[len_s1] = s2[i];
-		i++;
-		len_s1++;
-	}
-	str[len_s1] = '\0';
+	j = -1;
+	while (s2[++j])
+		str[i++] = s2[j];
+	str[size] = '\0';
 	return (str);
 }
